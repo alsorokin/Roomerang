5af1a202d6fb838976d5da0dde0b5797eddbabd0
Aleksandr Sorokin <me@snay.me>
2024-03-29 11:19:06 +0400

    Disable dragging and context menu

--- roomerang.client/src/main.tsx
+++ roomerang.client/src/main.tsx
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+// Disable right-click context menu
+window.addEventListener("contextmenu", e => e.preventDefault());
+
+// Disable dragging of all images... and everything else
+document.addEventListener("dragstart", function (e) {
+    e.preventDefault();
+});
